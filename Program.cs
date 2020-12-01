21bd5a59c5511e936350457d1acbb2b39ee87d25
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-01 17:22:17 +0000

    Better solution, aided by Dylan Beattie's Livestream

--- Program.cs
+++ Program.cs
@@ -11,27 +11,26 @@ namespace advent_of_code_2020
         {
             // Get a list of number values from the input file
             string inputFile = "data/day/1/input.txt";
-            List<int> values = File.ReadLines(inputFile).Select(line => int.Parse(line)).ToList();
+            IEnumerable<int> values = File.ReadAllLines(inputFile).Select(line => int.Parse(line));
 
             // Get the two numbers that add up to 2020
-            var sum = 2020;
-            int[] results = values.Where(value => values.Contains(sum - value)).ToArray();
-            Console.WriteLine("Matching rows: " + string.Join(' ', results));
-            if (results.Length == 2) {
-                Console.WriteLine("Part 1 Answer: " + (results[0] * results[1]));
-            }
+            Console.WriteLine("Part 1: " + Solve(values, 2, 2020));
 
             // Get the three numbers that add up to 2020
-            values.Sort();
-            for (var firstIndex = 0; firstIndex < values.Count() - 2 && values[firstIndex + 2] <= sum - (values[firstIndex] + values[firstIndex + 1]); firstIndex++) {
-                for (var secondIndex = firstIndex + 1; secondIndex < values.Count() - 1 && values[secondIndex + 1] <= sum - (values[firstIndex] + values[secondIndex]); secondIndex++) {
-                    var valueToFind = sum - (values[firstIndex] + values[secondIndex]);
-                    if (values.Where(value => value == valueToFind).Count() > 0) {
-                        Console.WriteLine("Matching values: " + values[firstIndex] + " " + values[secondIndex] + " " + valueToFind);
-                        Console.WriteLine("Part 2 Answer: " + (values[firstIndex] * values[secondIndex] * valueToFind));
-                    }
-                }
+            Console.WriteLine("Part 2: " + Solve(values, 3, 2020));
+        }
+
+        static int Solve(IEnumerable<int> ints, int howManyIntsToSum, int requiredSum) {
+            if (howManyIntsToSum == 0 || !ints.Any()) return 0;
+            var head = ints.First();
+            var tail = ints.Skip(1);
+            if (howManyIntsToSum == 1) {
+                if (head == requiredSum) return head;
+            } else {
+                var result = Solve(tail, howManyIntsToSum - 1, requiredSum - head);
+                if (result != 0) return head * result;
             }
+            return Solve(tail, howManyIntsToSum, requiredSum);
         }
     }
 }
