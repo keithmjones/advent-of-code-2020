70a4ee2268936de837f00ba7afd9956920d63ab6
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-01 11:42:45 +0000

    Day 1 Part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace advent_of_code_2020
 {
@@ -6,7 +9,16 @@ namespace advent_of_code_2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Get a list of number values from the input file
+            string inputFile = "data/day/1/input.txt";
+            List<int> values = File.ReadLines(inputFile).Select(line => int.Parse(line)).ToList();
+
+            // Get the two numbers that add up to 2020
+            int[] results = values.Where(value => values.Contains(2020 - value)).ToArray();
+            Console.WriteLine("Matching rows: " + string.Join(' ', results));
+            if (results.Length == 2) {
+                Console.WriteLine("Part 1 Answer: " + (results[0] * results[1]));
+            }
         }
     }
 }
