12b79c8e5bfa59a96f0cd94da235a1ee3af72155
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-01 13:20:11 +0000

    Day 1 Part 2

--- Program.cs
+++ Program.cs
@@ -14,10 +14,23 @@ namespace advent_of_code_2020
             List<int> values = File.ReadLines(inputFile).Select(line => int.Parse(line)).ToList();
 
             // Get the two numbers that add up to 2020
-            int[] results = values.Where(value => values.Contains(2020 - value)).ToArray();
+            var sum = 2020;
+            int[] results = values.Where(value => values.Contains(sum - value)).ToArray();
             Console.WriteLine("Matching rows: " + string.Join(' ', results));
             if (results.Length == 2) {
                 Console.WriteLine("Part 1 Answer: " + (results[0] * results[1]));
+            }
+
+            // Get the three numbers that add up to 2020
+            values.Sort();
+            for (var firstIndex = 0; firstIndex < values.Count() - 2 && values[firstIndex + 2] <= sum - (values[firstIndex] + values[firstIndex + 1]); firstIndex++) {
+                for (var secondIndex = firstIndex + 1; secondIndex < values.Count() - 1 && values[secondIndex + 1] <= sum - (values[firstIndex] + values[secondIndex]); secondIndex++) {
+                    var valueToFind = sum - (values[firstIndex] + values[secondIndex]);
+                    if (values.Where(value => value == valueToFind).Count() > 0) {
+                        Console.WriteLine("Matching values: " + values[firstIndex] + " " + values[secondIndex] + " " + valueToFind);
+                        Console.WriteLine("Part 2 Answer: " + (values[firstIndex] * values[secondIndex] * valueToFind));
+                    }
+                }
             }
         }
     }
