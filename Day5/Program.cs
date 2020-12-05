b3d0d0d7cf55dce40780d0ceb7011547d8d89cb0
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-05 18:51:54 +0000

    We can do this in fewer lines

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -10,28 +10,20 @@ namespace Day5
         static void Main(string[] args)
         {
             // Get a list of number values from the input file
+            // Seat numbers are binary, MSB first, where F/L=0, B/R=1
             string inputFile = "data/day/5/input.txt";
-            List<int> seats = File.ReadLines(inputFile).Select(
+            List<int> seats = File.ReadAllLines(inputFile).Select(
                 line => Convert.ToInt32(line.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2)
             ).ToList();
-            seats.Sort();
 
             // Part 1 answer
-            int max = seats.Max();
-            Console.WriteLine("Part 1 answer: {0}", max);
+            // Find highest numbered seat
+            Console.WriteLine("Part 1 answer: {0}", seats.Max());
 
             // Part 2 answer
             // Find unoccupied seat where s-1 and s+1 are occupied
-            int mySeat = -1;
-            int lastSeat = -1;
-            foreach (int seat in seats) {
-                if (lastSeat > 0 && seat == lastSeat + 2) {
-                    mySeat = seat - 1;
-                    Console.WriteLine("Part 2 answer: {0}", mySeat);
-                } else {
-                    lastSeat = seat;
-                }
-            }
+            seats.Sort();
+            Console.WriteLine("Part 2 answer: {0}", seats.Where((seat, index) => index > 0 && seats[index - 1] == seats[index] - 2).Single() - 1);
         }
     }
 }
