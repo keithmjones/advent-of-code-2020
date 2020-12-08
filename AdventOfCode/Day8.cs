1fd9894ebede6f9b64c023562970ec2cd0ab73ef
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-08 23:34:57 +0000

    Completed day 8 (sans unit tests)

---
+++ AdventOfCode/Day8.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day8: Day
+    {
+        private string[] data;
+
+        public void ReadInputFile(string inputFile)
+        {
+            data = File.ReadAllLines(inputFile).ToArray();
+        }
+
+        public int SolvePart1() => SolvePart1(data);
+
+        public int SolvePart2() => SolvePart2(data);
+
+        public int SolvePart1(string[] data) {
+            (var terminated, var accumulator, var programCounter) = Run(data);
+            return accumulator;
+        }
+
+        public int SolvePart2(string[] data) {
+            var totalJumpInstructions = data.Where(value => value.StartsWith("jmp")).Count();
+            for (var i = 0; i < totalJumpInstructions; i++)
+            {
+                (var terminated, var accumulator, var programCounter) = Run(data, i);
+                if (terminated)
+                {
+                    Console.Error.WriteLine("T={0} A={1} PC={2} LEN={3} J={4}", terminated, accumulator, programCounter, data.Length, i);
+                    return accumulator;
+                }
+            }
+            Console.Error.WriteLine("Program did not terminate :-(");
+            return 0;
+        }
+
+        public (bool, int, int) Run(string[] program, int jumpInstructionToIgnore = -1)
+        {
+            var visited = new bool[data.Length];
+            var terminated = false;
+            var accumulator = 0;
+            var programCounter = 0;
+            var nextJumpInstruction = 0;
+            do
+            {
+                visited[programCounter] = true;
+                string[] instruction = data[programCounter].Split(' ');
+                int value = Int32.Parse(instruction[1].Substring(1));
+                if (instruction[1][0] == '-')
+                {
+                    value = -value;
+                }
+                if (instruction[0] == "acc")
+                {
+                    accumulator += value;
+                }
+                if (instruction[0] == "jmp")
+                {
+                    programCounter += nextJumpInstruction == jumpInstructionToIgnore ? 1 : value;
+                    nextJumpInstruction++;
+                }
+                else
+                {
+                    programCounter++;
+                }
+                terminated = programCounter < 0 || programCounter >= data.Length;
+            } while (!terminated && !visited[programCounter]);
+            return (terminated, accumulator, programCounter);
+       }
+    }
+}
