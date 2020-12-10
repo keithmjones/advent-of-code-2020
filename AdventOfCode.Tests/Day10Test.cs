846a122b3ea83edfda6b1cc6b12d1cfb5a89704b
TechieKeith <techiekeith@users.noreply.github.com>
2020-12-10 15:29:26 +0000

    Day 10 Part 2

--- AdventOfCode.Tests/Day10Test.cs
+++ AdventOfCode.Tests/Day10Test.cs
@@ -30,7 +30,14 @@ namespace AdventOfCode.Tests
             new Int64[]
             {
                 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4
-            }, 0)]
+            }, 8)]
+        [InlineData(
+            new Int64[]
+            {
+                28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23,
+                49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17,
+                7, 9, 4, 2, 34, 10, 3
+            }, 19208)]
         public void CanSolvePart2(Int64[] ints, int expected)
         {
             var day10 = new Day10();
--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -7,34 +7,64 @@ namespace AdventOfCode
 {
     public class Day10: Day
     {
-        private Int64[] input;
+        private IEnumerable<Int64> input;
 
         public void ReadInputFile(string inputFile)
         {
-            input = File.ReadAllLines(inputFile).Select(value => Int64.Parse(value)).ToArray();
+            input = File.ReadAllLines(inputFile).Select(value => Int64.Parse(value));
         }
 
-        public Int64 SolvePart1() => SolvePart1(input);
+        public Int64 SolvePart1() => SolvePart1(input.ToArray());
 
-        public Int64 SolvePart2() => SolvePart2(input);
+        public Int64 SolvePart2() => SolvePart2(input.ToArray());
 
-        public Int64 SolvePart1(Int64[] ints)
+        Int64[] TopAndTail(IEnumerable<Int64> ints) {
+            Int64[] array = ints.Prepend(0).Append(ints.Max() + 3).ToArray();
+            Array.Sort(array);
+            return array;
+        }
+
+        public Int64 SolvePart1(IEnumerable<Int64> ints)
         {
-            Array.Sort(ints);
+            var array = TopAndTail(ints);
             var ones = 0;
             var threes = 0;
             Int64 last = 0;
-            foreach (var value in ints)
+            foreach (var value in array)
             {
                 var difference = value - last;
                 if (difference == 1) ones++;
                 if (difference == 3) threes++;
                 last = value;
             }
-            threes++; // our device is rated 3 jolts higher than highest rated adapter
             return ones * threes;
         }
 
-        public Int64 SolvePart2(Int64[] ints) => 0;
+        public Int64 SolvePart2(IEnumerable<Int64> ints)
+        {
+            var array = TopAndTail(ints);
+            Int64 last = 0;
+            var chain = 0;
+            var thisChainPermutations = 1;
+            Int64 totalPermutations = 1;
+            foreach (var value in array)
+            {
+                var difference = value - last;
+                if (difference == 1 || difference == 2)
+                {
+                    thisChainPermutations += chain;
+                    chain++;
+                }
+                else if (chain != 0)
+                {
+                    totalPermutations *= thisChainPermutations;
+                    chain = 0;
+                    thisChainPermutations = 1;
+                }
+                Console.WriteLine("{0} -> {1} [{2}]: {3} {4} {5}", last, value, difference, chain, thisChainPermutations, totalPermutations);
+                last = value;
+            }
+            return totalPermutations;
+        }
     }
 }
